f464ccf2c20fc428b8c770b7b6a59c5fb59d0a1e
Muoti5 <mbathamuoti19@students.ku.ac.ke>
2023-06-15 04:06:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ * Description: The puts function will print the string of characters
+ * in the brackets
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
