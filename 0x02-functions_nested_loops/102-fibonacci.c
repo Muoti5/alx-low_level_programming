b2b84e657d3513d76e7f1920c3bd98c3be5a3f87
Muoti5 <mbathamuoti19@students.ku.ac.ke>
2023-06-21 06:25:46 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,46 +1,32 @@
 #include <stdio.h>
 
 /**
-  *main - prints the first 98 Fibonacci numbers, starting with 1 and 2,
-  *followed by a new line.
+  *main - entry point
   *
-  *Return: complete
+  *Description: prints the first 50 Fibonacci numbers,
+  *starting with 1 and 2, followed by a new line
+  *
+  *Return: 0
   */
 
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int b = 1;
-	unsigned long int a = 2;
-	unsigned long int z = 1000000000;
-	unsigned long int b1;
-	unsigned long int b2;
-	unsigned long int a1;
-	unsigned long int a2;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	printf("%lu", b);
-
-	for (i = 1; i < 91; i++)
+	for (count = 0; count < 50; count++)
 	{
-		printf(", %lu", a);
-		a = a + b;
-		b = a - b;
-	}
-	b1 = (b / z);
-	b2 = (b % z);
-	a1 = (a / z);
-	a2 = (a % z);
+		sum = fib1 + fib2;
+		printf("%lu", sum);
 
-	for (i = 92; i < 99; i++)
-	{
-		printf(", %lu", a1 + (a2 / z));
-		printf("%lu", a2 % z);
-		a1 = a1 + b1;
-		b1 = a1 - b1;
-		a2 = a2 + b2;
-		b2 = a2 - b2;
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
 	}
 
-	printf("\n");
 	return (0);
 }
