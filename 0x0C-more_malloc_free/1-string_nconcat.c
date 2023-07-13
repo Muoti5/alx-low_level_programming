b9ee9c26f1c52d677c1ba5453214ff026a6b7577
Muoti5 <mbathamuoti19@students.ku.ac.ke>
2023-07-13 14:33:55 -0700

    nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - This function calculates the length of a string.
+ * @s: String
+ * Return: The number of bytes in the string pointed to by s.
+ */
+unsigned int _strlen(char *s)
+{
+	int a = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		s++;
+
+		a = a + _strlen(s);
+	}
+
+	return (a);
+}
+
+/**
+ * string_nconcat - This function concatenates two strings
+ *
+ * @s1: The destination string
+ * @s2: The source string
+ * @n: number of bytes in the source string
+ *
+ * Return: NULL if the function fails || 0 indicating success
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int a = _strlen(s1);
+	unsigned int len = _strlen(s2);
+	unsigned int b;
+	char *space;
+
+	if (s2 == NULL)
+		return ("");
+	if (s1 == NULL)
+		return ("");
+
+	space = malloc(len + a + 1);
+
+	if (space == NULL)
+		return (NULL);
+
+	for (a = 0; s1[a] != '\0'; a++)
+		space[a] = s1[a];
+	for (b = 0; b < n && s2[b] != '\0'; b++)
+		space[a + b] = s2[b];
+
+	space[a + b] = '\0';
+
+	return (space);
+}
