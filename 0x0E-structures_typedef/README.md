63365bd9da4975e79f0aa75ddfa521bf0411bb12
Muoti5 <mbathamuoti19@students.ku.ac.ke>
2023-07-17 09:52:24 -0700

    Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Structures make life fun
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure with the following elements
+ * @name: first member
+ * @age: second member
+ * @owner: Third member
+ *
+ * Description: structure dog has three elements as shown below
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
